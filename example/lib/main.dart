7d99f6183e8c3923470523b48d7f84e38ee615ea
Denis Dobanda <denis.dobanda@exxeta.com>
2024-10-24 08:04:33 +0200

    fix: fix example app layout overflow
    Signed-off-by: Denis Dobanda <denis.dobanda@exxeta.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -58,41 +58,44 @@ class _MyAppState extends State<MyApp> {
           child: Center(
             child: Builder(builder: (context) {
               final textTheme = Theme.of(context).textTheme;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    _jailbroken || _hooks ? Icons.lock_open : Icons.lock,
-                    size: 80,
-                  ).padding(bottom: 24),
-                  Text(
-                    'Protection',
-                    style: textTheme.headlineLarge,
-                  ).padding(bottom: 8),
-                  Text(
-                    'Here is a list of the threats that could put you at risk',
-                    style: textTheme.titleMedium?.copyWith(color: Colors.grey),
-                    textAlign: TextAlign.center,
-                  ).padding(bottom: 16),
-                  ThreatCard(
-                    title: 'Jailbreak / Root',
-                    description:
-                        'Is a way of acquiring privileged control over the operating system of a device. Tools such as Magisk or Shadow can hide the privileged access',
-                    status: _jailbroken,
-                  ),
-                  ThreatCard(
-                    title: 'Hooks',
-                    description:
-                        'Intercept system or application calls and then modify them (modify the return value of a function call for example)',
-                    status: _hooks,
-                  ),
-                  ThreatCard(
-                    title: 'Simulator',
-                    description: 'Running the application in an Simulator',
-                    status: _simulator,
-                  ),
-                ],
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      _jailbroken || _hooks ? Icons.lock_open : Icons.lock,
+                      size: 80,
+                    ).padding(bottom: 24),
+                    Text(
+                      'Protection',
+                      style: textTheme.headlineLarge,
+                    ).padding(bottom: 8),
+                    Text(
+                      'Here is a list of the threats that could put you at risk',
+                      style:
+                          textTheme.titleMedium?.copyWith(color: Colors.grey),
+                      textAlign: TextAlign.center,
+                    ).padding(bottom: 16),
+                    ThreatCard(
+                      title: 'Jailbreak / Root',
+                      description:
+                          'Is a way of acquiring privileged control over the operating system of a device. Tools such as Magisk or Shadow can hide the privileged access',
+                      status: _jailbroken,
+                    ),
+                    ThreatCard(
+                      title: 'Hooks',
+                      description:
+                          'Intercept system or application calls and then modify them (modify the return value of a function call for example)',
+                      status: _hooks,
+                    ),
+                    ThreatCard(
+                      title: 'Simulator',
+                      description: 'Running the application in an Simulator',
+                      status: _simulator,
+                    ),
+                  ],
+                ),
               );
             }),
           ).padding(left: 20, right: 20),
