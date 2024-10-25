923eac894cec499404d138268d7b3468b6f53477
Denis Dobanda <denis.dobanda@exxeta.com>
2024-10-25 16:56:12 +0200

    docs: added documentation of public methods
    Signed-off-by: Denis Dobanda <denis.dobanda@exxeta.com>

--- lib/src/threat_detection_center.dart
+++ lib/src/threat_detection_center.dart
@@ -1,14 +1,25 @@
 import 'flutter_security_toolkit_platform_interface.dart';
 
+/// Use this class to access the methods of detecting security threats
+/// Note: most methods are async
 class ThreatDetectionCenter {
+  /// Will check for Jailbreak on iOS and for Root on Android.
+  ///
+  /// Returns true, if privileged access detected.
   static Future<bool?> areRootPrivilegesDetected() {
     return FlutterSecurityToolkitPlatform.instance.areRootPrivilegesDetected();
   }
 
+  /// Will check for runtime integrity by detecting hooks / dynamic injection
+  /// libraries.
+  ///
+  /// Returns true, if any hooks are detected
   static Future<bool?> areHooksDetected() {
     return FlutterSecurityToolkitPlatform.instance.areHooksDetected();
   }
 
+  /// Will check for an environment and return true, if running in simulated /
+  /// emulated environment
   static Future<bool?> isSimulatorDetected() {
     return FlutterSecurityToolkitPlatform.instance.isSimulatorDetected();
   }
